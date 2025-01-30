a587d022df86e861b543fe792557e389a4eb0182
S. T <64820224+aTongs1@users.noreply.github.com>
2025-01-30 12:42:41 +0800

    Add files via upload

---
+++ datasets/__init__.py
@@ -0,0 +1,7 @@
+from .cub200 import CUB200
+from .stanford_dogs import StanfordDogs
+from .dr import DRDataset
+from .dr_012 import DRDataset012
+from .dr_34 import DRDataset34
+from .aptos import APTOSDataset
+from .sicapv2 import SICAPv2Dataset
